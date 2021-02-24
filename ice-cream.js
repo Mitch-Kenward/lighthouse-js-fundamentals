aa83cb560274688c9a6f1403f1b54f77592f7d05
Mitch-Kenward <mkenward29@gmail.com>
2021-02-24 16:44:26 +0000

    "An array of flavor

--- ice-cream.js
+++ ice-cream.js
@@ -4,5 +4,8 @@ console.log(iceCreamFlavours);
 
 // Your code below here...
 iceCreamFlavours.push("root beer");
-
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours.length);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
 console.log(iceCreamFlavours.length);
